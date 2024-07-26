3c5fad7e53fb1fda8139be391a21d016e2b71a12
casjay <git-admin@casjaysdev.pro>
2024-07-26 16:40:08 -0400

    🗃️ Committing everything that changed 🗃️
    config/php/
    config/php-fpm/
    config/php-fpm.conf
    config/php-fpm.d/www.conf
    config/php.ini
    install.sh

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ WWW_ROOT_DIR="${WWW_ROOT_DIR:-/usr/share/httpd/default}"
 INIT_DIR="/usr/local/etc/docker/init.d"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set this if a file should exist - comma seperated list
-CONFIG_CHECK_FILE="$CONFIG_DIR/php.ini"
+CONFIG_CHECK_FILE="$CONFIG_DIR/php/php.ini"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 GIT_REPO="https://github.com/templatemgr/$TEMPLATE_NAME"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -134,7 +134,8 @@ fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # custom operations
-[ "$TEMPLATE_NAME" = "php" ] && { [ -f "/config/env/php.sh" ] || echo "PHP_DEV_SERVER_START=\"${PHP_DEV_SERVER_START:-yes}\"" >/config/env/php.sh; }
+[ -d "/usr/local/share/template-files/config/env" ] || mkdir -p "/usr/local/share/template-files/config/env"
+[ "$TEMPLATE_NAME" = "php" ] && { [ -f "/usr/local/share/template-files/config/env/php.sh" ] || echo "PHP_DEV_SERVER_START=\"${PHP_DEV_SERVER_START:-yes}\"" >/usr/local/share/template-files/config/env/php.sh; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 CONFIG_CHECK_FILE="${CONFIG_CHECK_FILE//,/ }"
 if [ -n "$CONFIG_CHECK_FILE" ]; then
